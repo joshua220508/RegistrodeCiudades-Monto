80a41f6627701e92307da454b94fb1f155e44cf1
joshua220508 <jesaenz@uamv.edu.ni>
2025-09-22 13:53:02 -0600

    Add project files.

---
+++ CiudadesApp/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace CiudadesApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbNombre = new System.Windows.Forms.TextBox();
+            this.tbPoblacion = new System.Windows.Forms.TextBox();
+            this.dtpFundacion = new System.Windows.Forms.DateTimePicker();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.dgvRegistros = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvRegistros)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(72, 92);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(72, 162);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Poblacion:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(72, 226);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(88, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Fundacion:";
+            // 
+            // tbNombre
+            // 
+            this.tbNombre.Location = new System.Drawing.Point(178, 92);
+            this.tbNombre.Name = "tbNombre";
+            this.tbNombre.Size = new System.Drawing.Size(317, 26);
+            this.tbNombre.TabIndex = 3;
+            // 
+            // tbPoblacion
+            // 
+            this.tbPoblacion.Location = new System.Drawing.Point(178, 162);
+            this.tbPoblacion.Name = "tbPoblacion";
+            this.tbPoblacion.Size = new System.Drawing.Size(168, 26);
+            this.tbPoblacion.TabIndex = 4;
+            // 
+            // dtpFundacion
+            // 
+            this.dtpFundacion.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpFundacion.Location = new System.Drawing.Point(178, 226);
+            this.dtpFundacion.Name = "dtpFundacion";
+            this.dtpFundacion.Size = new System.Drawing.Size(133, 26);
+            this.dtpFundacion.TabIndex = 5;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(178, 287);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(134, 39);
+            this.btnAgregar.TabIndex = 6;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // dgvRegistros
+            // 
+            this.dgvRegistros.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvRegistros.Location = new System.Drawing.Point(76, 352);
+            this.dgvRegistros.Name = "dgvRegistros";
+            this.dgvRegistros.RowHeadersWidth = 62;
+            this.dgvRegistros.RowTemplate.Height = 28;
+            this.dgvRegistros.Size = new System.Drawing.Size(419, 166);
+            this.dgvRegistros.TabIndex = 7;
+            this.dgvRegistros.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvRegistros_CellContentClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(563, 530);
+            this.Controls.Add(this.dgvRegistros);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.dtpFundacion);
+            this.Controls.Add(this.tbPoblacion);
+            this.Controls.Add(this.tbNombre);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Ciudades";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvRegistros)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbNombre;
+        private System.Windows.Forms.TextBox tbPoblacion;
+        private System.Windows.Forms.DateTimePicker dtpFundacion;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.DataGridView dgvRegistros;
+    }
+}
+
---
+++ CiudadesLibrary/Dao/CiudadDao.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CiudadesLibrary.Modelos;
+
+namespace CiudadesLibrary.Dao
+{
+    public class CiudadDao
+    {
+        private Ciudad[] listado = new Ciudad[10];
+        private int pos=0;
+
+        public void AgregarCiudad(Ciudad ciudad)
+        {
+            try
+            {
+                listado[pos] = ciudad;
+                pos++;
+            } catch (IndexOutOfRangeException ex)
+            {
+                Console.WriteLine("No se puede agregar mas de 10 elementos");
+            }
+        }
+        public Ciudad[] MostrarCiudad()
+        { 
+            return listado; 
+        }
+
+    }
+}
